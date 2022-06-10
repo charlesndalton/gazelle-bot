396416267330177ee9dc298eeedbf6f4303e5bb4
charlesndalton <charlesndalton@gmail.com>
2022-06-09 22:41:07 -0500

    feat: get some more stuff working

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ async fn main() -> Result<()> {
     let infura_api_key = env::var("INFURA_API_KEY").expect("INFURA_API_KEY not set");
 
     let report = report_creator::create_report(&infura_api_key).await?;
-    println!("REPORT: {:?}", report);
-
     report_publisher::publish_report(report, &telegram_token).await?;
 
     Ok(())
@@ -19,47 +17,47 @@ async fn main() -> Result<()> {
 
 mod report_creator {
     use crate::{
+        exchange_rate_api_client,
         graph_client,
-        types::{Result, Error, AngleReport, IndividualAssetAngleReport}
+        types::{Result, Error, AngleStablecoinReport, CollateralReport}
     };
-    use std::fs::File;
-    use std::io::prelude::*;
     use serde_json::{Value};
-    use ethers::abi::Address;
-    use rust_decimal_macros::dec;
-    use rust_decimal::Decimal;
     use num_bigint::BigInt;
     use bigdecimal::{BigDecimal, One};
     use std::str::FromStr;
 
-    pub async fn create_report(infura_api_key: &str) -> Result<AngleReport> {
+    pub async fn create_report(infura_api_key: &str) -> Result<AngleStablecoinReport> {
         let mut collateral_reports = Vec::new();
 
-        let collateral_data = graph_client::get_collateral_data().await?;
+        let stablecoin_data = graph_client::get_stablecoin_data().await?;
         // hack: unwraps galore
-        let pool_datas = collateral_data["data"]["poolDatas"].as_array().unwrap();
+        println!("{:?}", stablecoin_data);
+        let ag_eur_data = &stablecoin_data["data"]["stableDatas"].as_array().unwrap()[0];
+        let total_minted = BigDecimal::from_str(ag_eur_data["totalMinted"].as_str().unwrap())?;
+        //let total_minted_value = BigDecimal::from(1);
+        //let total_minted = BigDecimal::from(1);
+        let total_minted_value = exchange_rate_api_client::convert_eur_to_usd(&total_minted).await?;
 
-        for pool_data in pool_datas {
-            let pool_data = pool_data.as_object().unwrap();
+        let collateral_datas = ag_eur_data["collaterals"].as_array().unwrap();
 
-            if pool_data["stableName"].as_str().unwrap() != "agEUR" {
-                continue; // doesn't matter for now but in case they introduce another stablecoin
-            }
+        for collateral_data in collateral_datas {
+            let collateral_data = collateral_data.as_object().unwrap();
 
-            println!("{:?}", pool_data);
 
-            let decimals: u32 = pool_data["decimals"].as_str().unwrap().parse().unwrap();
+            println!("{:?}", collateral_data);
+
+            let decimals: u32 = collateral_data["decimals"].as_str().unwrap().parse().unwrap();
             let slp_divide_by: BigDecimal = (BigInt::one() * 10_u32).pow(18 + decimals).into(); // they format the stockSLP in 18 + decimals
-            let stock_slp = BigDecimal::from_str(pool_data["stockSLP"].as_str().unwrap())? / slp_divide_by;
+            let stock_slp = BigDecimal::from_str(collateral_data["stockSLP"].as_str().unwrap())? / slp_divide_by;
 
-            let stock_user = BigDecimal::from_str(pool_data["stockUser"].as_str().unwrap())? / 10_i64.pow(18); 
-            let total_hedge_amount = BigDecimal::from_str(pool_data["totalHedgeAmount"].as_str().unwrap())? / 10_i64.pow(18);
+            let stock_user = BigDecimal::from_str(collateral_data["stockUser"].as_str().unwrap())? / 10_i64.pow(18); 
+            let total_hedge_amount = BigDecimal::from_str(collateral_data["totalHedgeAmount"].as_str().unwrap())? / 10_i64.pow(18);
             let hedge_ratio = ((total_hedge_amount / &stock_user) * BigDecimal::from(100));
         
-            collateral_reports.push(IndividualAssetAngleReport::new(pool_data["collatName"].as_str().unwrap().to_string(), hedge_ratio.with_scale(0), stock_user.with_scale(0), stock_slp.with_scale(0)));
+            collateral_reports.push(CollateralReport::new(collateral_data["collatName"].as_str().unwrap().to_string(), hedge_ratio.with_scale(0), stock_user.with_scale(0), stock_slp.with_scale(0)));
         }
 
-        Ok(AngleReport::new(BigDecimal::from(1), BigDecimal::from(1), collateral_reports)) 
+        Ok(AngleStablecoinReport::new(total_minted.with_scale(0), total_minted_value.with_scale(0), BigDecimal::from(1), BigDecimal::from(1), collateral_reports)) 
     }
 }
 
@@ -69,40 +67,57 @@ mod graph_client {
     // this is very hacky (using string post body and `Value` response instead of using structs &
     // deserializing) but I will hopefully be the only one to ever interact with this bot
 
-    const collateral_data_url: &str = "https://api.thegraph.com/subgraphs/name/picodes/transaction";
-    const collateral_data_post_body: &str = "{ \"query\": \"{ poolDatas { stableName, collatName, decimals, stockUser, totalHedgeAmount, stockSLP } }\" }";
+    const URL: &str = "https://api.thegraph.com/subgraphs/name/picodes/transaction";
+    const COLLATERAL_DATA_POST_BODY: &str = "{ \"query\": \"{ poolDatas { stableName, collatName, decimals, stockUser, totalHedgeAmount, stockSLP } }\" }";
+    const STABLECOIN_DATA_POST_BODY: &str = "{ \"query\": \"{ stableDatas { name, totalMinted, collatRatio, collaterals { collatName, decimals, stockSLP, stockUser, totalHedgeAmount } } }\" }";
 
-    pub async fn get_collateral_data() -> Result<Value> {
+    //pub async fn get_collateral_data() -> Result<Value> {
+    //    let client = reqwest::Client::new();
+
+    //    let res = client.post(COLLATERAL_DATA_URL)
+    //        .body(COLLATERAL_DATA_POST_BODY)
+    //        .send()
+    //        .await?;
+
+    //    let res_json: Value = res.json().await?;
+
+    //    Ok(res_json)
+    //}
+
+    pub async fn get_stablecoin_data() -> Result<Value> {
         let client = reqwest::Client::new();
 
-        let res = client.post(collateral_data_url)
-            .body(collateral_data_post_body)
+        let response = client.post(URL)
+            .body(STABLECOIN_DATA_POST_BODY)
             .send()
             .await?;
 
-        let res_json: Value = res.json().await?;
+        let response_json: Value = response.json().await?;
 
-        Ok(res_json)
+        Ok(response_json)
     }
 }
+
 
 mod report_publisher {
     use crate::{
         telegram_client,
-        types::{Result, AngleReport}
+        types::{Result, AngleStablecoinReport}
     };
 
-    pub async fn publish_report(report: AngleReport, telegram_token: &str) -> Result<()> {
+    pub async fn publish_report(report: AngleStablecoinReport, telegram_token: &str) -> Result<()> {
         println!("Daily Angle");
         println!("-----------");
+        println!("Total agEUR minted: {} (${})", report.total_minted(), report.total_minted_value());
         println!("Total collateralization ratio: {}", report.total_collateralization_ratio());
         println!("Organic collateralization ratio: {}", report.organic_collateralization_ratio());
-        for individual_asset_report in report.collateral_reports() {
+        for collateral_report in report.collateral_reports() {
             println!("-----------");
-            println!("Asset – {}", individual_asset_report.collateral_asset());
+            println!("Asset – {}", collateral_report.asset_name());
+            println!("Percentage of volatility hedged: {}", collateral_report.hedge_ratio());
             println!("Percentage of total TVL: x");
-            println!("User TVL: {}", individual_asset_report.user_tvl());
-            println!("SLP TVL: {}", individual_asset_report.slp_tvl());
+            println!("User TVL: {}", collateral_report.user_tvl());
+            println!("SLP TVL: {}", collateral_report.slp_tvl());
             //telegram_client::send_message_to_committee(format!("{}'s price risk relative to the euro is hedged away {}%", individual_report.collateral_asset(), individual_report.collateralization_ratio()).as_str(), telegram_token).await?;
             //
         }
@@ -110,7 +125,6 @@ mod report_publisher {
         Ok(())
     }
 }
-
 
 mod telegram_client {
     use crate::types::Result;
@@ -128,6 +142,28 @@ mod telegram_client {
     }
 }
 
+mod exchange_rate_api_client {
+    use crate::types::Result;
+    use bigdecimal::BigDecimal;
+    use std::str::FromStr;
+    use serde_json::{Value};
+
+    pub async fn convert_eur_to_usd(amount: &BigDecimal) -> Result<BigDecimal> {
+        let url = format!("https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount={}", amount);
+
+        let client = reqwest::Client::new();
+        let response = client
+            .get(url)
+            .header("apikey", "3ZjsVwEmO2v4cOOLl87N8VVev5yeD5GK")
+            .send()
+            .await?;
+
+        let response_json: Value = response.json().await?;
+        println!("{:?}", response_json);
+
+        Ok(BigDecimal::from(response_json["result"].as_i64().unwrap()))
+    }
+}
 mod types {
     use derive_getters::{Getters};
     use derive_new::new;
@@ -136,53 +172,38 @@ mod types {
     #[derive(Debug, thiserror::Error)]
     pub enum Error {
         #[error(transparent)]
-        EthrsContractError(#[from] ethers::contract::ContractError<ethers::providers::Provider<ethers::providers::Http>>),
+        UrlParse(#[from] url::ParseError),
 
         #[error(transparent)]
-        UrlParseError(#[from] url::ParseError),
+        SerdeJson(#[from] serde_json::Error),
 
         #[error(transparent)]
-        AddressParseStringToHexError(#[from] rustc_hex::FromHexError),
+        Reqwest(#[from] reqwest::Error),
 
         #[error(transparent)]
-        EyreReport(#[from] eyre::Report),
+        ParseBigInt(#[from] num_bigint::ParseBigIntError),
 
         #[error(transparent)]
-        IOError(#[from] std::io::Error),
-
-        #[error(transparent)]
-        SerdeJsonError(#[from] serde_json::Error),
-
-        #[error(transparent)]
-        ReqwestError(#[from] reqwest::Error),
-
-        #[error(transparent)]
-        ParseBigIntError(#[from] num_bigint::ParseBigIntError),
-
-        #[error(transparent)]
-        ParseBigDecimalError(#[from] bigdecimal::ParseBigDecimalError),
-
-        #[error("We expected to find the following field {expected_field:?} in ethereum.json, but it wasn't there.")]
-        EthAddressesError {
-            expected_field: String,
-        },
+        ParseBigDecimal(#[from] bigdecimal::ParseBigDecimalError),
     }
 
     pub type Result<T> = std::result::Result<T, Error>;
 
     #[derive(Getters, new, Debug)]
-    pub struct IndividualAssetAngleReport {
-        collateral_asset: String,
+    pub struct CollateralReport { 
+        asset_name: String,
         hedge_ratio: BigDecimal,
         user_tvl: BigDecimal,
         slp_tvl: BigDecimal,
     }
 
     #[derive(Getters, new, Debug)]
-    pub struct AngleReport {
+    pub struct AngleStablecoinReport {
+        total_minted: BigDecimal,
+        total_minted_value: BigDecimal,
         organic_collateralization_ratio: BigDecimal,
         total_collateralization_ratio: BigDecimal,
-        collateral_reports: Vec<IndividualAssetAngleReport>
+        collateral_reports: Vec<CollateralReport>,
     }
 
 }
