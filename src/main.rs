50adfa1e9f6aa1717a5698dc5c39cbe4ab8b4e22
charlesndalton <charlesndalton@gmail.com>
2022-06-10 12:55:25 -0700

    feat: get some more reporting working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use types::Result;
 use std::env;
-use std::time::{Duration};
-use std::thread;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let telegram_token = env::var("GAZELLE_TELEGRAM_TOKEN").expect("GAZELLE_TELEGRAM_TOKEN not set");
     let infura_api_key = env::var("INFURA_API_KEY").expect("INFURA_API_KEY not set");
+
+
+    println!("Starting!");
 
     let report = report_creator::create_report(&infura_api_key).await?;
     report_publisher::publish_report(report, &telegram_token).await?;
@@ -18,6 +19,7 @@ async fn main() -> Result<()> {
 mod report_creator {
     use crate::{
         exchange_rate_api_client,
+        cryptocurrency_prices_api_client,
         graph_client,
         types::{Result, Error, AngleStablecoinReport, CollateralReport}
     };
@@ -33,31 +35,43 @@ mod report_creator {
         // hack: unwraps galore
         println!("{:?}", stablecoin_data);
         let ag_eur_data = &stablecoin_data["data"]["stableDatas"].as_array().unwrap()[0];
-        let total_minted = BigDecimal::from_str(ag_eur_data["totalMinted"].as_str().unwrap())?;
+        let total_minted = (BigDecimal::from_str(ag_eur_data["totalMinted"].as_str().unwrap())? / 10_i64.pow(18)).with_scale(0);
         //let total_minted_value = BigDecimal::from(1);
         //let total_minted = BigDecimal::from(1);
-        let total_minted_value = exchange_rate_api_client::convert_eur_to_usd(&total_minted).await?;
+        let exchange_rate = exchange_rate_api_client::get_eur_usd_exchange_rate().await?;
+        let total_minted_value = &total_minted * exchange_rate; 
+        let mut total_collateral_value = BigDecimal::from(0);
+        let mut organic_collateral_value = BigDecimal::from(0); // not incl. SLP deposits
 
         let collateral_datas = ag_eur_data["collaterals"].as_array().unwrap();
 
         for collateral_data in collateral_datas {
             let collateral_data = collateral_data.as_object().unwrap();
 
-
-            println!("{:?}", collateral_data);
-
             let decimals: u32 = collateral_data["decimals"].as_str().unwrap().parse().unwrap();
             let slp_divide_by: BigDecimal = (BigInt::one() * 10_u32).pow(18 + decimals).into(); // they format the stockSLP in 18 + decimals
-            let stock_slp = BigDecimal::from_str(collateral_data["stockSLP"].as_str().unwrap())? / slp_divide_by;
+            let stock_slp = (BigDecimal::from_str(collateral_data["stockSLP"].as_str().unwrap())? / slp_divide_by).with_scale(0);
 
-            let stock_user = BigDecimal::from_str(collateral_data["stockUser"].as_str().unwrap())? / 10_i64.pow(18); 
-            let total_hedge_amount = BigDecimal::from_str(collateral_data["totalHedgeAmount"].as_str().unwrap())? / 10_i64.pow(18);
+            let stock_user = (BigDecimal::from_str(collateral_data["stockUser"].as_str().unwrap())? / 10_i64.pow(18)).with_scale(0); 
+            let total_hedge_amount = (BigDecimal::from_str(collateral_data["totalHedgeAmount"].as_str().unwrap())? / 10_i64.pow(18)).with_scale(0);
             let hedge_ratio = ((total_hedge_amount / &stock_user) * BigDecimal::from(100));
+
+            let collateral_symbol = collateral_data["collatName"].as_str().unwrap();
+            let price = cryptocurrency_prices_api_client::get_usd_price(collateral_symbol).await?;
+            let stock_user_value = &stock_user * &price;
+            let stock_slp_value = &stock_slp * &price;
+
+            organic_collateral_value += &stock_user_value;
+            total_collateral_value += (&stock_user_value + &stock_slp_value);
         
-            collateral_reports.push(CollateralReport::new(collateral_data["collatName"].as_str().unwrap().to_string(), hedge_ratio.with_scale(0), stock_user.with_scale(0), stock_slp.with_scale(0)));
+            collateral_reports.push(CollateralReport::new(collateral_symbol.to_string(), hedge_ratio.with_scale(0), stock_user.with_scale(0), stock_user_value.with_scale(0), stock_slp.with_scale(0), stock_slp_value.with_scale(0)));
         }
 
-        Ok(AngleStablecoinReport::new(total_minted.with_scale(0), total_minted_value.with_scale(0), BigDecimal::from(1), BigDecimal::from(1), collateral_reports)) 
+        let organic_collateralization_ratio = &organic_collateral_value / &total_minted_value; 
+        let total_collateralization_ratio = &total_collateral_value / &total_minted_value;
+
+
+        Ok(AngleStablecoinReport::new(total_minted.with_scale(0), total_minted_value.with_scale(0), organic_collateral_value.with_scale(0), total_collateral_value.with_scale(0), organic_collateralization_ratio.with_scale(2), total_collateralization_ratio.with_scale(2), collateral_reports)) 
     }
 }
 
@@ -104,6 +118,7 @@ mod report_publisher {
         telegram_client,
         types::{Result, AngleStablecoinReport}
     };
+    use bigdecimal::BigDecimal;
 
     pub async fn publish_report(report: AngleStablecoinReport, telegram_token: &str) -> Result<()> {
         println!("Daily Angle");
@@ -114,10 +129,10 @@ mod report_publisher {
         for collateral_report in report.collateral_reports() {
             println!("-----------");
             println!("Asset – {}", collateral_report.asset_name());
-            println!("Percentage of volatility hedged: {}", collateral_report.hedge_ratio());
-            println!("Percentage of total TVL: x");
-            println!("User TVL: {}", collateral_report.user_tvl());
-            println!("SLP TVL: {}", collateral_report.slp_tvl());
+            println!("Percentage of volatility hedged: {}%", collateral_report.hedge_ratio());
+            println!("Percentage of organic TVL: {}%", ((collateral_report.user_tvl_value() / report.organic_tvl()) * BigDecimal::from(100)).with_scale(0));
+            println!("User TVL: {}, (${})", collateral_report.user_tvl(), collateral_report.user_tvl_value());
+            println!("SLP TVL: {}, (${})", collateral_report.slp_tvl(), collateral_report.slp_tvl_value());
             //telegram_client::send_message_to_committee(format!("{}'s price risk relative to the euro is hedged away {}%", individual_report.collateral_asset(), individual_report.collateralization_ratio()).as_str(), telegram_token).await?;
             //
         }
@@ -142,28 +157,80 @@ mod telegram_client {
     }
 }
 
+mod cryptocurrency_prices_api_client {
+    use crate::types::{Result, Error};
+    use bigdecimal::BigDecimal;
+    use std::str::FromStr;
+    use serde_json::{Value};
+    use backoff::{ExponentialBackoff, 
+        future::retry};
+
+    // How many USD is one EUR worth?
+    pub async fn get_usd_price(symbol: &str) -> Result<BigDecimal> {
+        let url = format!("https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol={}", symbol);
+        Ok(retry(ExponentialBackoff::default(), || async {
+            let client = reqwest::Client::new();
+
+            let response = client
+                .get(&url)
+                .header("X-CMC_PRO_API_KEY", "61e35d42-f985-45d5-880d-b93a5f76164b")
+                .send()
+                .await?;
+            let response: Value = response.json().await?;
+            println!("{:?}", response);
+
+            let usd_price_raw = response["data"][symbol].as_array().unwrap()[0]["quote"]["USD"]["price"].as_f64().unwrap();
+            let usd_price = BigDecimal::from((usd_price_raw * 1_000.0).round() as i64) / 1_000;
+            println!("price: {}", usd_price);
+
+            Ok(usd_price)
+        }).await?)
+    }
+}
+
 mod exchange_rate_api_client {
     use crate::types::Result;
     use bigdecimal::BigDecimal;
     use std::str::FromStr;
     use serde_json::{Value};
 
-    pub async fn convert_eur_to_usd(amount: &BigDecimal) -> Result<BigDecimal> {
-        let url = format!("https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount={}", amount);
+    const EXCHANGE_RATE_URL: &str = "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount=1";
 
+    // How many USD is one EUR worth?
+    pub async fn get_eur_usd_exchange_rate() -> Result<BigDecimal> {
         let client = reqwest::Client::new();
         let response = client
-            .get(url)
+            .get(EXCHANGE_RATE_URL)
             .header("apikey", "3ZjsVwEmO2v4cOOLl87N8VVev5yeD5GK")
             .send()
             .await?;
+        let response: Value = response.json().await?;
 
-        let response_json: Value = response.json().await?;
-        println!("{:?}", response_json);
+        // hack: From<f64> not implemented for BigDecimal
+        let exchange_rate_raw = response["result"].as_f64().unwrap();
+        let exchange_rate = BigDecimal::from((exchange_rate_raw * 1_000.0).round() as i64) / 1_000;
+        println!("er: {}", exchange_rate);
 
-        Ok(BigDecimal::from(response_json["result"].as_i64().unwrap()))
+        Ok(exchange_rate)
     }
+
+    //pub async fn convert_eur_to_usd(amount: &BigDecimal) -> Result<BigDecimal> {
+    //    let url = format!("https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount={}", amount);
+
+    //    let client = reqwest::Client::new();
+    //    let response = client
+    //        .get(url)
+    //        .header("apikey", "3ZjsVwEmO2v4cOOLl87N8VVev5yeD5GK")
+    //        .send()
+    //        .await?;
+
+    //    let response_json: Value = response.json().await?;
+    //    println!("{:?}", response_json);
+
+    //    Ok(BigDecimal::from(response_json["result"].as_f64().unwrap().round() as i64))
+    //}
 }
+
 mod types {
     use derive_getters::{Getters};
     use derive_new::new;
@@ -194,13 +261,17 @@ mod types {
         asset_name: String,
         hedge_ratio: BigDecimal,
         user_tvl: BigDecimal,
+        user_tvl_value: BigDecimal,
         slp_tvl: BigDecimal,
+        slp_tvl_value: BigDecimal,
     }
 
     #[derive(Getters, new, Debug)]
     pub struct AngleStablecoinReport {
         total_minted: BigDecimal,
         total_minted_value: BigDecimal,
+        organic_tvl: BigDecimal,
+        total_tvl: BigDecimal,
         organic_collateralization_ratio: BigDecimal,
         total_collateralization_ratio: BigDecimal,
         collateral_reports: Vec<CollateralReport>,
